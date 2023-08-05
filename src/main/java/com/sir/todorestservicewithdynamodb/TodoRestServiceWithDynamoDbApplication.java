9ed9f93912840ab7971724fb45819a1f46e34560
Saiful Islam Rasel <raselcse97@gmail.com>
2023-08-05 15:30:12 +0600

    initialize project

---
+++ src/main/java/com/sir/todorestservicewithdynamodb/TodoRestServiceWithDynamoDbApplication.java
@@ -0,0 +1,13 @@
+package com.sir.todorestservicewithdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestServiceWithDynamoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestServiceWithDynamoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sir/todorestservicewithdynamodb/TodoRestServiceWithDynamoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sir.todorestservicewithdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestServiceWithDynamoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
